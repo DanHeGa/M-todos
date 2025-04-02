0e4538372e4631ad9958443a4de58cca82b8493c
DanielaHG <A00838702@tec.mx>
2025-04-01 23:26:14 -0600

    added floats but still need E and - identification

--- test.py
+++ test.py
@@ -2,9 +2,7 @@ linea = "78 a 4 b6"
 l2 = "a = 32.4 *(-8.6 - b)/       6.1E-8"
 l3 = "6.8 75 6 c"
 
-
-'''hora puedes manejar que SOLO haya positivos enteros, pero
-teidentifica el 8 del flotante con expo, ahi entra la identificación de flotantes'''
+'''Ya verifica flotante, ahora necesita identificar si son negativos y el uso de exponenciales E y su exponente'''
 lo = list(l2)
 entero = []
 for i in range(len(lo)):
@@ -14,9 +12,10 @@ for i in range(len(lo)):
             if lo[i + 1] == '.':
                 entero.append(lo[i + 1])
             else:
-                if '.' in entero:
+                if '.' in entero: #float identification
+                    flt = "".join(entero)
+                    print(f"{flt} float")
                     entero = []
-                    pass
                 else:
                     numero = "".join(entero)
                     print(f"{numero} Int")
