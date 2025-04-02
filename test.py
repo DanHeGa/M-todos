239a470ddec10a600b015d7b3db25b003c85ce0e
DanielaHG <A00838702@tec.mx>
2025-04-02 15:39:29 -0600

    added negative ints and exponential floats

--- test.py
+++ test.py
@@ -1,16 +1,23 @@
 linea = "78 a 4 b6"
 l2 = "a = 32.4 *(-8.6 - b)/       6.1E-8"
-l3 = "6.8 75 6 c"
+l3 = "-6.8e75 -6 c"
 
 '''Ya verifica flotante, ahora necesita identificar si son negativos y el uso de exponenciales E y su exponente'''
-lo = list(l2)
+lo = list(linea)
 entero = [] #commnt
 for i in range(len(lo)):
-    if lo[i].isdigit():
+    if lo[i] == '-' and lo[i + 1].isdigit():
+        entero.append(lo[i])
+    elif lo[i].isdigit():
         entero.append(lo[i])
         if i < len(lo) - 1 and not lo[i + 1].isdigit():
             if lo[i + 1] == '.':
                 entero.append(lo[i + 1])
+            elif lo[i + 1] in 'Ee': #identificar exponencial
+                entero.append(lo[i + 1])
+                if i + 2 < len(lo) and (lo[i + 2].isdigit() or lo[i + 2] == '-'):
+                    while i + 1 < len(lo) and lo[i + 1].isdigit():
+                        entero.append(lo[i + 1])
             else:
                 if '.' in entero: #float identification
                     flt = "".join(entero)
@@ -21,8 +28,11 @@ for i in range(len(lo)):
                     print(f"{numero} Int")
                     entero = []
         elif i == len(lo) - 1:
-           numero = "".join(entero)
-           print(f"{numero} Int")
+            numero = "".join(entero)
+            if '.' in numero:
+               print(f"{numero} float")
+            else:
+                print(f"{numero} Int")
     else:
         continue
   
