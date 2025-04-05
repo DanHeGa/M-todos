4ffc7f24e28659ed0c427966b206821e6b791aa4
DanielaHG <A00838702@tec.mx>
2025-04-04 20:39:39 -0600

    lastest changes

--- test.py
+++ test.py
@@ -1,37 +1,48 @@
 linea = "78 a 4 b6"
 l2 = "a = 32.4 *(-8.6 - b)/       6.1E-8"
 l3 = "-6.8e75 -6 c"
+l4 = "a_MyVar03=32.4*(-8.6 - b_var8)/       6.1e-8 //Esto es un = comentario"
 
-lo = list(linea)
+lo = l4
 entero = [] #number mngt
-for i in range(len(lo)):
+i = 0
+while i < len(lo):
     if lo[i] == '-' and lo[i + 1].isdigit():
         entero.append(lo[i])
-    elif lo[i].isdigit():
+        i += 1
+    if lo[i].isdigit():
         entero.append(lo[i])
         if i < len(lo) - 1 and not lo[i + 1].isdigit():
             if lo[i + 1] == '.':
                 entero.append(lo[i + 1])
-            elif lo[i + 1] in 'Ee': #identificar exponencial
-                entero.append(lo[i + 1])
-                if i + 2 < len(lo) and (lo[i + 2].isdigit() or lo[i + 2] == '-'):
-                    while i + 1 < len(lo) and lo[i + 1].isdigit():
+                i += 1
+                while i + 1 < len(lo) and lo[i + 1].isdigit():
+                    i += 1
+                    entero.append(lo[i])
+                if lo[i + 1] in 'Ee': #identificar exponencial
+                    entero.append(lo[i + 1])
+                    i += 1
+                    if i + 1 < len(lo) and (lo[i + 1].isdigit() or lo[i + 1] == '-'):
                         entero.append(lo[i + 1])
+                        i += 1
+                        while i + 1 < len(lo) and lo[i + 1].isdigit():
+                            i += 1
+                            entero.append(lo[i])
+            num = "".join(entero)           
+            if '.' in num or 'E' in num or 'e' in num:
+                print(f"{num} float")
             else:
-                if '.' in entero: #float identification
-                    flt = "".join(entero)
-                    print(f"{flt} float")
-                    entero = []
-                else:
-                    numero = "".join(entero)
-                    print(f"{numero} Int")
-                    entero = []
+                print(f"{num} Int")
+            entero = []
         elif i == len(lo) - 1:
             numero = "".join(entero)
             if '.' in numero:
                print(f"{numero} float")
             else:
                 print(f"{numero} Int")
+            entero = []          
     else:
+        i += 1
         continue
+    i += 1
   
