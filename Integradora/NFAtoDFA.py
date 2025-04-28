473188195bf7e00f991d67c83ffcd1f784be81b2
DanielaHG <A00838702@tec.mx>
2025-04-28 17:35:30 -0600

    made NFA2DFA modular

--- Integradora/NFAtoDFA.py
+++ Integradora/NFAtoDFA.py
@@ -10,33 +10,21 @@ originalNFA = [
 ]
 
 '''
-# Esta es la salida del NFA de Dany:
-keys = ['e', 'B', 'C', 'A']
-originalNFA = [{'A': 1}, {'e': 5}, {'B': 3}, {'e': 5}, 
-               {'e': [0, 2]}, {'e': 8}, {'C': 7}, {'e': [6, 9]}, 
-               {'e': [6, 9]}, {}]
-
-
-
-# Cambia el NFA de la salida anterior a una tabla con las transiciones
-# Guarda como listas de destinos
-nfa = {}
-for i, transiciones in enumerate(originalNFA):
-    nfa[i] = {}
-    for sym, dest in transiciones.items():
-        if isinstance(dest, int):
-            nfa[i][sym] = [dest]
-        else:
-            nfa[i][sym] = list(dest)
-
-# Define el alfabeto, omitiendo el 'e' del simbolo de entrada
-# Alfabeto sin 'e'
-alfabeto = [k for k in keys if k != 'e']
-startState = 0
-acceptState = {i for i, transiciones in enumerate(originalNFA) if not transiciones}
+def convert_to_tableNFA(originalNFA):
+    # Cambia el NFA de la salida anterior a una tabla con las transiciones
+    # Guarda como listas de destinos
+    nfa = {}
+    for i, transiciones in enumerate(originalNFA):
+        nfa[i] = {}
+        for sym, dest in transiciones.items():
+            if isinstance(dest, int):
+                nfa[i][sym] = [dest]
+            else:
+                nfa[i][sym] = list(dest)
+    return nfa
 
 # Encuentra los estados alcanzables por transiciones epsilon
-def transicionesEpsilon(states):
+def transicionesEpsilon(nfa, states):
     stack = list(states)
     closure = set(states)
 
@@ -50,64 +38,96 @@ def transicionesEpsilon(states):
     return closure
 
 # Encuentra los estados alcanzables por simbolos especificos (que nos sean epsilon)
-def transicionesSimbolos(states, symbol):
+def transicionesSimbolos(nfa, states, symbol):
     result = set()
     for state in states:
         for dest in nfa.get(state, {}).get(symbol, []):
             result.add(dest)
     return result
 
-# Define variables para empezar la conversion
-dfaStates = []
-dfaTransitions = {}
-dfaFinalState = []
-visto = set()
+def nfa_to_dfa(originalNFA, keys):
+     # Define el alfabeto, omitiendo el 'e' del simbolo de entrada
+    # Alfabeto sin 'e'
+    nfa = convert_to_tableNFA(originalNFA)
+    alfabeto = [k for k in keys if k != 'e']
+    startState = 0
+    acceptState = {i for i, transiciones in enumerate(originalNFA) if not transiciones}
+    
+    # Define variables para empezar la conversion
+    dfaStates = []
+    dfaTransitions = {}
+    dfaFinalState = []
+    visto = set()
 
-# Usar para el estado inicial la funcion transicionesEpsilon
-start = frozenset(transicionesEpsilon([startState]))
-queue = deque([start])
-visto.add(start)
+    # Usar para el estado inicial la funcion transicionesEpsilon
+    start = frozenset(transicionesEpsilon(nfa, [startState]))
+    queue = deque([start])
+    visto.add(start)
 
-# Aplica el algoritmo de subconjuntos
-while queue:
-    current = queue.popleft()
-    dfaStates.append(current)
+    # Aplica el algoritmo de subconjuntos
+    while queue:
+        current = queue.popleft()
+        dfaStates.append(current)
 
-    for symbol in alfabeto: # Se intenta mover con cada simbolo del alfabeto
-        movimiento = transicionesSimbolos(current, symbol)
-        estadosAlcanzables = transicionesEpsilon(movimiento) # aplica transicionesEpsilon despues de cada movimiento
-        conjuntoEstados = frozenset(estadosAlcanzables)
+        for symbol in alfabeto: # Se intenta mover con cada simbolo del alfabeto
+            movimiento = transicionesSimbolos(nfa, current, symbol)
+            estadosAlcanzables = transicionesEpsilon(nfa, movimiento) # aplica transicionesEpsilon despues de cada movimiento
+            conjuntoEstados = frozenset(estadosAlcanzables)
+            dfaTransitions[(current, symbol)] = conjuntoEstados # Conjunto de transiciones para el DFA
 
-        dfaTransitions[(current, symbol)] = conjuntoEstados # Conjunto de transiciones para el DFA
+            # Agregar conjuntoEstados a la fila si no se ha visto, para procesar despues
+            if conjuntoEstados not in visto:
+                visto.add(conjuntoEstados)
+                queue.append(conjuntoEstados)
 
-        # Agregar conjuntoEstados a la fila si no se ha visto, para procesar despues
-        if conjuntoEstados not in visto:
-            visto.add(conjuntoEstados)
-            queue.append(conjuntoEstados)
+    # Toma como estado final del AFD cualquier conjunto de estados del DFA que contenga el estado final del AFN
+    for state in dfaStates:
+        if any(s in acceptState for s in state):
+            dfaFinalState.append(state)
 
+    resultDFA = {
+        "estados": dfaStates,
+        "transiciones": dfaTransitions,
+        "estado_inicial": start,
+        "estados_finales": dfaFinalState
+    }
+    return resultDFA
 
-# Toma como estado final del AFD cualquier conjunto de estados del DFA que contenga el estado final del AFN
-for state in dfaStates:
-    if any(s in acceptState for s in state):
-        dfaFinalState.append(state)
+def print_DFA(dfaStates, dfaTransitions, start, dfaFinalState):
+    # Imprime resultados
+    imprimeEstados = []
+    for i in range(len(dfaStates)):
+        imprimeEstados.append("Q" + str(i) + ":" + str(sorted(dfaStates[i])))
+    print("Estados del AFD: " + " , ".join(imprimeEstados))
 
+    imprimeTransiciones = []
+    for key in dfaTransitions:
+        src = sorted(list(key[0]))
+        sym = key[1]
+        dest = sorted(dfaTransitions[key])
+        imprimeTransiciones.append(str(src) + " --" + str(sym) + "--> " + str(dest))
+    print("Transiciones del AFD: " + " , ".join(imprimeTransiciones))
 
-# Imprime resultados
-imprimeEstados = []
-for i in range(len(dfaStates)):
-    imprimeEstados.append("Q" + str(i) + ":" + str(sorted(dfaStates[i])))
-print("Estados del AFD: " + " , ".join(imprimeEstados))
+    print("Estado inicial del AFD: " + str(sorted(start)))
 
-imprimeTransiciones = []
-for key in dfaTransitions:
-    src = sorted(list(key[0]))
-    sym = key[1]
-    dest = sorted(dfaTransitions[key])
-    imprimeTransiciones.append(str(src) + " --" + str(sym) + "--> " + str(dest))
-print("Transiciones del AFD: " + " , ".join(imprimeTransiciones))
+    imprimeEstadosFinales = [str(sorted(state)) for state in dfaFinalState]
+    print("Estados finales del AFD: " + " , ".join(imprimeEstadosFinales))
 
-print("Estado inicial del AFD: " + str(sorted(start)))
+def main():   
+    # Esta es la salida del NFA de Dany:
+    keys = ['e', 'B', 'C', 'A']
+    originalNFA = [{'A': 1}, {'e': 5}, {'B': 3}, {'e': 5}, 
+                {'e': [0, 2]}, {'e': 8}, {'C': 7}, {'e': [6, 9]}, 
+                {'e': [6, 9]}, {}]
 
-imprimeEstadosFinales = [str(sorted(state)) for state in dfaFinalState]
-print("Estados finales del AFD: " + " , ".join(imprimeEstadosFinales))
+    dfa = nfa_to_dfa(originalNFA, keys)
 
+    states = dfa["estados"]
+    transitions = dfa["transiciones"]
+    initial_state = dfa["estado_inicial"]
+    final_state = dfa["estados_finales"]
+
+    print_DFA(states, transitions, initial_state, final_state)
+    
+if __name__ == '__main__':
+    main()
