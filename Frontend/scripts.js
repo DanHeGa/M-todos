61129624dade46e1aeed1d56a5a6e77f0f6ffd87
Emily Joanne Castillo Martinez <143458810+emilyjcastillom@users.noreply.github.com>
2025-05-02 08:25:28 -0600

    Update scripts.js

--- Frontend/scripts.js
+++ Frontend/scripts.js
@@ -1,11 +1,19 @@
 async function getData() {
-    try {
-      const response = await fetch('http://localhost:2000/api/registros/generales');
-      if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
-      const data = await response.json();
-      document.getElementById("output").textContent = JSON.stringify(data, null, 2);
-    } catch (err) {
-      document.getElementById("output").textContent = "❌ Error al obtener datos: " + err.message;
-    }
+  try {
+    const alfabeto = document.getElementById("alfabeto").value;
+    const regex = document.getElementById("ER").value;
+
+    const response = await fetch('/convertor', {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ regex, alfabeto })
+    });
+
+    if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
+    const data = await response.json();
+
+    document.getElementById("output").innerHTML = data.svg; // Mostrar SVG
+  } catch (err) {
+    document.getElementById("output").textContent = "Error: " + err.message;
   }
-  
+}
